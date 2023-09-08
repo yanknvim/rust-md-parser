9302ad1884c257b1dff9b4937282c49e0c13e109
yanknvim <yanknvim@gmail.com>
2023-09-08 15:48:17 +0900

    Add converter to html

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod tokenizer;
 mod parser;
+mod to_html;
 
 fn main() {
     let md = "# h1\n## h2\n*italic*\n**bold**\n# *test*".to_string();
@@ -7,6 +8,6 @@ fn main() {
     println!("{:?}", tokens);
     for token_line in tokens {
         let ast = parser::parse(token_line);
-        println!("{:?}", ast);
+        println!("{}", to_html::to_html(ast));
     }
 }
--- src/to_html.rs
+++ src/to_html.rs
@@ -1,15 +1,25 @@
-use crate::parser::{Node, NodeKind};
+use crate::parser::Node;
 
 pub fn to_html(ast: Vec<Node>) -> String {
     let mut html = String::new();
     for node in ast {
-        match node.kind {
-            NodeKind::Text => {
-                html.push_str(&node.str);
+        match node {
+            Node::Text(text) => {
+                html.push_str(&text);
             },
-            NodeKind::Bold => {
-                html.push_str(&format!("<strong>{}</strong>", )
-            }
+            Node::Bold(child) => {
+                html.push_str(&format!("<strong>{}</strong>", to_html(child)));
+            },
+            Node::Italic(child) => {
+                html.push_str(&format!("<em>{}</em>", to_html(child)));
+            },
+            Node::Header(depth, child) => {
+                html.push_str(&format!("<h{}>{}</h{}>", depth, to_html(child), depth));
+            },
+            Node::Blockquote(child) => {
+                html.push_str(&format!("<blockquote>{}</blockquote>", to_html(child)));
+            },
+            _ => {},
         }
     }
     html
